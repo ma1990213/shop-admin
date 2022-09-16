c5c68b78e01a0315c44e2d730869da1a5fca4645
maqiruo <maqr@bbdtek.com>
2022-09-16 17:09:57 +0800

    mmp

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './style.css';
-import App from './App.vue';
+import './style.css'
+import App from './App.vue'
 
-const tset = '123';--
+=====
 
 createApp(App).mount('#app')
